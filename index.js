54d566d3dc22279d4630aa713f124a7624a8067e
Evan03 <Evan.onihotmail.com>
2023-04-04 01:33:25 -0400

    added dom events to create shape of card

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-let myLibrary = [];
+let myLibrary = [
+  {title:'How to kill a mocking bird', author:' Harper Lee', pages:'342', read:'No'},
+  {title:'48 Law of power', author:'Robert Greene', pages:'400', read:'No'},
+  {title:'Perfect day to boss up', author:'Rick Ross', pages:'200', read:'No'},
+  {title:'Can not hurt me', author:'David Goggins', pages:'340', read:'Yes'}
+
+
+];
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -21,10 +28,41 @@ function addBookToLibrary() {
 }
 
 function displayBook() {
-  for (let book of myLibrary) {
-    console.log(book.name, book.authour, book.pages, book.read);
-  }
+
+
+  const container= document.getElementById('card')
+  container.innerHTML='';
+  myLibrary.forEach(Book => {
+     
+
+    const card = document.createElement("div");
+    card.classList.add("card")
+    
+    const title = document.createElement("h1")
+    title.textContent=  `${Book.title}`
+
+    const author = document.createElement("p");
+    author.textContent =`${Book.author}`;
+
+    const pages = document.createElement("p");
+    pages.textContent = `${Book.pages}`;
+
+
+const read = document.createElement("p");
+  read.textContent = Book.read;
+
+
+  card.appendChild(title);
+  card.appendChild(author);
+  card.appendChild(pages);
+  card.appendChild(read);
+  container.appendChild(card)
+  });
+
+
+
 }
 
 //Add to the table using dom methods in javascript
 //add a new book button
+displayBook()
